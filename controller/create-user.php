45de6f5edb6404767286a4d1ffc4276e9e3e56b3
Brandon Obermeit <bladegx4@gmail.com>
2015-02-18 14:12:19 -0800

    .

--- controller/create-user.php
+++ controller/create-user.php
@@ -1,8 +1,23 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+    require_once(__DIR__ . "/../model/config.php");
+    
+    $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+    $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_EMAIL);
+    $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_EMAIL);
+    
+    $salt = "$5$" . "rounds=5ooo$" . uniqid(mt_rand(), true) . "$";
+    
+    $hashedPassword = crypt($password, $salt);
+    
+    $query = $_SESION["connection"]->query("INSERT INTO users SET "
+            . "emaill = '$email',"
+            . "username = '$username',"
+            . "password = '$hashedPassword',"
+            . "salt = '$salt'");
+    
+    if($query) {
+        echo "Successfully created user: $username";
+    }
+    else {
+        echo "<p>" . $_SESSION["connection"]->error . "</p>";
+    }
